d6cd2c0b64cc290712a8899828a7287204eb2b78
SMB <sethmichaelbrown@gmail.com>
2018-12-13 16:08:13 -0700

    Adding comments to JS

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,16 +3,17 @@ document.addEventListener('DOMContentLoaded', function(event) {
   M.AutoInit();
 
 
-
+// Homepage Button - Random Text Gen
   const login = document.querySelector('.ride-btn')
   const btnText = document.querySelector('.btn-text')
 
   login.addEventListener('click', (event) => {
     let arr = ["Live With Us", "Groove With Us", "Jam With Us", "Vibe With Us", "Sing With Us", "Dance With Us", "Party With Us", "Get There With Us", "Save Earth With Us", "Rock With Us", "Bump With Us", "Roll With Us"]
-    let result = arr[Math.floor(Math.random()*arr.length)]
+    let result = arr[Math.floor(Math.random() * arr.length)]
 
     btnText.innerText = `${result}`
   })
+// End Homepage Button
 
 
 }) // DOMContentLoaded;
