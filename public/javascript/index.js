0b9ae1cf9bd3b63a98372bcfc49c8d480e195b9e
SMB <sethmichaelbrown@gmail.com>
2018-12-13 15:39:30 -0700

    Created homepage

---
+++ public/javascript/index.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+  console.log("It's Alive")
+  M.AutoInit();
+
+
+
+  const login = document.querySelector('.ride-btn')
+  const btnText = document.querySelector('.btn-text')
+
+  login.addEventListener('click', (event) => {
+    let arr = ["Live With Us", "Groove With Us", "Jam With Us", "Vibe With Us", "Sing With Us", "Dance With Us", "Party With Us", "Get There With Us", "Save Earth With Us", "Rock With Us", "Bump With Us", "Roll With Us"]
+    let result = arr[Math.floor(Math.random()*arr.length)]
+
+    btnText.innerText = `${result}`
+  })
+
+
+}) // DOMContentLoaded;
