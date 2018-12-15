0dca8e484452ab4aeac2bee2001c38b3ab80c8af
Anthony Pogliano <poglianoster@gmail.com>
2018-12-15 12:20:39 -0700

    Updating JS for book button modal.

--- public/javascript/calendar.js
+++ public/javascript/calendar.js
@@ -401,6 +401,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
       buttonDiv.setAttribute('class', 'col l2 s2 row-element valign-wrapper row-element-last')
       button.setAttribute('class', 'waves-effect waves-light btn event-btn z-depth-0 center valign-wrapper modal-trigger')
       button.setAttribute('data-target', 'modal3')
+      button.setAttribute('id', 'event-button')
 
 
       dateDiv.innerText = `${event.date}`
@@ -431,7 +432,46 @@ document.addEventListener('DOMContentLoaded', function(event) {
   // Book Function
   const bookModal = document.querySelector('#modal3-content')
 
+  // Location TEST ARRAY //
+  const pickupArr = [{
+    service: "BTS: Boulder",
+    time: '5:30pm',
+    location: 'UNIV. HILL CHEBA HUT',
+    headliner: 'BOOMBOX',
+    venue: 'THE FILLMORE',
+    roundTrip: true,
+    price: "$25.00",
+    saleEnd: "December 31",
+    fee: "$2.23"
+  }, {
+    service: "BTS: Boulder",
+    time: '5:30pm',
+    location: 'UNIV. HILL CHEBA HUT',
+    headliner: 'BOOMBOX',
+    venue: 'THE FILLMORE',
+    roundTrip: true,
+    price: "$25.00",
+    saleEnd: "December 31",
+    fee: "$2.23"
+  }, {
+    service: "BTS: Boulder",
+    time: '5:30pm',
+    location: 'UNIV. HILL CHEBA HUT',
+    headliner: 'BOOMBOX',
+    venue: 'THE FILLMORE',
+    roundTrip: false,
+    price: "$25.00",
+    saleEnd: "December 31",
+    fee: "$2.23"
+  }]
+
+
   calendar.addEventListener('click', (event) => {
+
+    while (bookModal.hasChildNodes()) {
+      bookModal.removeChild(bookModal.lastChild);
+    }
+
     let eventInfo = event.target.value
     let headliner = eventInfo.headliner
     let date = eventInfo.date
@@ -439,13 +479,41 @@ document.addEventListener('DOMContentLoaded', function(event) {
     let day = eventInfo.day
     let modalTitle = document.createElement('h5')
 
-    while (bookModal.hasChildNodes()) {
-      bookModal.removeChild(bookModal.lastChild);
-    }
-
     modalTitle.innerText = `${headliner} - ${day} (${date}) at ${venue}`
 
     bookModal.appendChild(modalTitle)
+
+    // USE WITH Location TEST ARRAY //
+    pickupArr.forEach(option => {
+      let service = option.service
+      let time = option.time
+      let location = option.location
+      let headliner = option.headliner
+      let venue = option.venue
+      let price = option.price
+      let saleEnd = option.saleEnd
+      let roundTrip = option.rountTrip
+      let fee = option.fee
+      let rTrip
+
+      option.roundTrip ? rTrip = '(and back)' : rTrip = ''
+
+
+
+      let row = document.createElement('div')
+      let pickupOption = document.createElement('div')
+
+      row.setAttribute('class', 'row')
+      pickupOption.setAttribute('class', 'pickupOpt col s12 l12')
+
+      pickupOption.innerText = `${service} | ${time} ${location} to ${headliner} at ${venue} ${rTrip} \n ${price} + ${fee} FEE \n ${saleEnd}`
+
+
+      modalTitle.appendChild(row)
+      row.appendChild(pickupOption)
+
+    })
+
 
 
   })
